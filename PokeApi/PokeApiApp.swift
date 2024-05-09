138309e918c69a4f494a4c418ccc50c31d64702f
ChristopherPeralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-05-09 13:02:50 -0500

    init

---
+++ PokeApi/PokeApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiApp.swift
+//  PokeApi
+//
+//  Created by Christopher Peralta on 9/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
